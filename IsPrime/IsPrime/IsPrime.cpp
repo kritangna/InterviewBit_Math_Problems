b38931f6fc70f2666d74749f52f45c9ecbcb61d3
kritangna <kritangna.singh.ext@sivantos.com>
2021-05-12 23:57:32 +0530

    fixed the for loop

--- IsPrime/IsPrime/IsPrime.cpp
+++ IsPrime/IsPrime/IsPrime.cpp
@@ -17,7 +17,7 @@ public:
 int Solution::isPrime(int num) {
 	if (num < 2)
 		return 0;
-	for (int i = 2; i < sqrt(num); i++) {
+	for (int i = 2; i <= sqrt(num); i++) {
 		if (num % i == 0)
 			return 0;
 	}
